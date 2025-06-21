f47e3421004ffa395c8f92c4cdebd7d47ad1b8e9
krionBl9nnhc93i <benprosen541@gmail.com>
2025-06-21 15:57:12 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -1,29 +1,15 @@
---[[
-
-  _    _ _ _         _                              __                                   _ 
- | |  (_) | |       (_)                            / _|                                 | |
- | | ___| | |  _ __  _  __ _  __ _  ___ _ __ ___  | |_ ___  _ __   _ __   _____      __ | |
- | |/ / | | | | '_ \| |/ _` |/ _` |/ _ \ '__/ __| |  _/ _ \| '__| | '_ \ / _ \ \ /\ / / | |
- |   <| | | | | | | | | (_| | (_| |  __/ |  \__ \ | || (_) | |    | | | | (_) \ V  V /  |_|
- |_|\_\_|_|_| |_| |_|_|\__, |\__, |\___|_|  |___/ |_| \___/|_|    |_| |_|\___/ \_/\_/   (_)
-                        __/ | __/ |                                                        
-                       |___/ |___/                                                         
-
-
-soon !
---]]
-
 local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/krionBl9nnhc93i/krionui/main/api.lua"))()
+local speedSelector = ui:Selector("Movement", "Speed", "Speed Mode", {"CFrame", "Velocity", "Walkspeed"}, 1, function(idx, val)
+    ui:Notify("Seçilen: " .. val)
+end)
 
-local speedSelector = ui:Selector(
-    "Movement", "Speed",
-    "Speed Mode",
-    {"CFrame", "Velocity", "Walkspeed"},
-    1,
-    function(idx, val)
-        ui:Notify("Seçilen: " .. val)
-    end
-)
+print("selector", speedSelector)
+print("get", speedSelector.get)
+print("set", speedSelector.set)
 
-local i, v = speedSelector:get()
-speedSelector:set(2)
+if speedSelector and speedSelector.get then
+    local i, v = speedSelector:get()
+    speedSelector:set(2)
+else
+    warn("Selector'da get() ve/veya set() yok!")
+end

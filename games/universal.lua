8845c970bfd977346f7d05d2fc5fdf9a848bc30e
krionBl9nnhc93i <benprosen541@gmail.com>
2025-06-21 16:32:41 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -9,3 +9,7 @@ local speedSelector = ui:Selector(
 print("speedSelector:", speedSelector)
 print("speedSelector.get:", speedSelector.get)
 print("speedSelector.set:", speedSelector.set)
+print("typeof:", typeof(speedSelector))
+print("metatable:", getmetatable(speedSelector))
+for k,v in pairs(speedSelector) do print("INSTANCE FIELD", k, v) end
+for k,v in pairs(getmetatable(speedSelector) or {}) do print("MT FIELD", k, v) end

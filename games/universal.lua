83d119877f66d9cd5f66ef8aa068f51173fd1d09
krionBl9nnhc93i <benprosen541@gmail.com>
2025-06-21 16:02:52 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -1,15 +1,11 @@
 local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/krionBl9nnhc93i/krionui/main/api.lua"))()
-local speedSelector = ui:Selector("Movement", "Speed", "Speed Mode", {"CFrame", "Velocity", "Walkspeed"}, 1, function(idx, val)
-    ui:Notify("Seçilen: " .. val)
-end)
+local speedSelector = ui:Selector(
+    "Movement", "Speed", "Speed Mode", {"CFrame", "Velocity", "Walkspeed"}, 1,
+    function(idx, val)
+        ui:Notify("Seçilen: " .. val)
+    end
+)
 
-print("selector", speedSelector)
-print("get", speedSelector.get)
-print("set", speedSelector.set)
-
-if speedSelector and speedSelector.get then
-    local i, v = speedSelector:get()
-    speedSelector:set(2)
-else
-    warn("Selector doesnt have get() and set()")
-end
+print("speedSelector:", speedSelector)
+print("speedSelector.get:", speedSelector.get)
+print("speedSelector.set:", speedSelector.set)
